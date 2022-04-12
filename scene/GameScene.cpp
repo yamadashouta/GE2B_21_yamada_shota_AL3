471d460ae8b42861ad374ca3c0c420041dd37b6c
620yama <k021g1398@g.neec.ac.jp>
2022-04-12 16:17:04 +0900

    01_02の確認課題をコミット

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,7 +6,11 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() {}
+GameScene::~GameScene() {
+//デストラクタ
+	delete sprite_;
+	delete model_;
+}
 
 void GameScene::Initialize() {
 
@@ -14,9 +18,51 @@ void GameScene::Initialize() {
 	input_ = Input::GetInstance();
 	audio_ = Audio::GetInstance();
 	debugText_ = DebugText::GetInstance();
+
+	//ファイル名を指定してテクスチャを読み込む
+	textureHandle_ = TextureManager::Load("mario.jpg");
+
+	//スプライトの生成
+	sprite_ = Sprite::Create(textureHandle_, {100, 50});
+
+	//3Dモデルの生成
+	model_ = Model::Create();
+
+	//ワールドトランスフォームの初期化
+	worldTransform_.Initialize();
+	//ビュープロジェクションの初期化
+	viewProjection_.Initialize();
+
+		//サウンドデータの読み込み
+	soundDateHandle_ = audio_->LoadWave("se_sad03.wav");
+
+	//音声再生
+	audio_->PlayWave(soundDateHandle_, true);
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	//スプライトの今の座標を取得
+	XMFLOAT2 position = sprite_->GetPosition();
+	//座標を{2, 0}移動
+	position.x += 2.0f;
+	position.y += 1.0f;
+	//移動した座標をスプライトに反映
+	sprite_->SetPosition(position);
+
+	//スペースキーを押した瞬間
+	if (input_->TriggerKey(DIK_SPACE)) {
+		//音声停止
+		audio_->StopWave(soundDateHandle_);
+	}
+
+	//変数の値をインクリメント
+	value_++;
+	//値を含んだ文字列
+	std::string strDebug = std::string("Value:") + std::to_string(value_);
+
+	//デバッグテキストの表示
+	debugText_->Print(strDebug, 50, 50, 1.0f);
+}
 
 void GameScene::Draw() {
 
@@ -44,6 +90,8 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
+	//3Dモデル描画
+	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
 	Model::PostDraw();
@@ -56,6 +104,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに前景スプライトの描画処理を追加できる
 	/// </summary>
+	sprite_->Draw();
 
 	// デバッグテキストの描画
 	debugText_->DrawAll(commandList);
--- scene/GameScene.h
+++ scene/GameScene.h
@@ -48,6 +48,26 @@ class GameScene {
 	Audio* audio_ = nullptr;
 	DebugText* debugText_ = nullptr;
 
+	//テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+
+	//スプライト
+	Sprite* sprite_ = nullptr;
+
+	//3Dモデル
+	Model* model_ = nullptr;
+
+	//ワールドトランスフォーム
+	WorldTransform worldTransform_;
+
+	//ビュープロジェクション
+	ViewProjection viewProjection_;
+
+	//サウンドデータハンドル
+	uint32_t soundDateHandle_ = 0;
+
+	//値を表示したい変数
+	int32_t value_ = 0;
 	/// <summary>
 	/// ゲームシーン用
 	/// </summary>
