a6360ce7b25e5fa74a06ad201ecab04cf754eead
620yama <k021g1398@g.neec.ac.jp>
2022-04-19 14:26:45 +0900

    デバッグテキストの表示

--- scene/GameScene.cpp
+++ scene/GameScene.cpp
@@ -6,9 +6,7 @@ using namespace DirectX;
 
 GameScene::GameScene() {}
 
-GameScene::~GameScene() { 
-	delete model_; 
-}
+GameScene::~GameScene() { delete model_; }
 
 void GameScene::Initialize() {
 
@@ -18,20 +16,19 @@ void GameScene::Initialize() {
 	debugText_ = DebugText::GetInstance();
 
 	//ファイル名を指定してテクスチャを読み込む
-	textureHandle_ = TextureManager::Load("mario.jpg");	
+	textureHandle_ = TextureManager::Load("mario.jpg");
 
-	//3Dモデルの生成
+	// 3Dモデルの生成
 	model_ = Model::Create();
 
 	// X, Y, Z 軸周りの平行移動を設定
 	worldTransform_.translation_ = {10.0f, 10.0f, 10.0f};
 
-	//X, Y, Z　軸周りの回転数を設定
+	// X, Y, Z　軸周りの回転数を設定
 	worldTransform_.rotation_ = {XM_PI / 4.0f, XM_PI / 4.0f, 0.0f};
 
 	// X, Y, Z 方向のスケーリングを設定
 	worldTransform_.scale_ = {5.0f, 5.0f, 5.0f};
-
 
 	//ワールドトランスフォームの初期化
 	worldTransform_.Initialize();
@@ -40,7 +37,15 @@ void GameScene::Initialize() {
 	viewProjection_.Initialize();
 }
 
-void GameScene::Update() {}
+void GameScene::Update() {
+	//デバッグテキストの書式指定付き表示
+	debugText_->SetPos(50, 70);
+	debugText_->Printf("translation:(%f,%f,%f)",worldTransform_.translation_.x, worldTransform_.translation_.y,worldTransform_.translation_.z);
+	debugText_->SetPos(50, 90);
+	debugText_->Printf("rotation:(%f,%f,%f)", worldTransform_.rotation_.x, worldTransform_.rotation_.y, worldTransform_.rotation_.z);
+	debugText_->SetPos(50, 110);
+	debugText_->Printf("scale:(%f,%f,%f)",worldTransform_.scale_.x, worldTransform_.scale_.y,worldTransform_.scale_.z);
+}
 
 void GameScene::Draw() {
 
@@ -68,7 +73,7 @@ void GameScene::Draw() {
 	/// <summary>
 	/// ここに3Dオブジェクトの描画処理を追加できる
 	/// </summary>
-	//3Dモデル描画
+	// 3Dモデル描画
 	model_->Draw(worldTransform_, viewProjection_, textureHandle_);
 
 	// 3Dオブジェクト描画後処理
